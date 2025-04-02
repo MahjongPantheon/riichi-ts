af784bc9e5a158698cd77d3646e88478d4425627
Oleg Klimenko <me@ctizen.dev>
2025-04-03 02:45:02 +0300

    migrate to yarn

--- riichi_realdata_rs.test.ts
+++ riichi_realdata_rs.test.ts
@@ -11,7 +11,7 @@ let total = 0;
 let logsProcessed = 0;
 const failedLogs: string[] = [];
 
-const toRrsOne = (inp: number) => inp + 1 as Tile;
+const toRrsOne = (inp: number) => (inp + 1) as Tile;
 const toRrs = (inp: number[]) => inp.map(toRrsOne);
 
 describe('Real games data', () => {
@@ -29,13 +29,14 @@ describe('Real games data', () => {
           for (const idx in hands) {
             const data = hands[idx];
             try {
-
               const options = {
                 bakaze: toRrsOne(data.bakaze),
                 jikaze: toRrsOne(data.jikaze),
                 dora: toRrs(data.doraTiles),
-                tile_discarded_by_someone: data.takenTile === undefined ? -1 : toRrsOne(data.takenTile),
-                first_take: data.yakuman.includes('tenhou') ||
+                tile_discarded_by_someone:
+                  data.takenTile === undefined ? -1 : toRrsOne(data.takenTile),
+                first_take:
+                  data.yakuman.includes('tenhou') ||
                   data.yakuman.includes('chihou') ||
                   data.yakuman.includes('renhou'),
                 riichi: data.yaku.includes('riichi'),
@@ -62,8 +63,8 @@ describe('Real games data', () => {
                   return o as Meld;
                 }),
                 options,
-                calc_hairi: false
-              })
+                calc_hairi: false,
+              });
 
               if (data.yakuman.length === 0) {
                 assert.equal(res.fu, data.fu);
@@ -97,62 +98,119 @@ describe('Real games data', () => {
 
 function toYakuNames(y: string): string {
   switch (parseInt(y, 10) as Yaku) {
-    case Yaku.Kokushimusou13Sides: return 'kokushimusou 13 sides';
-    case Yaku.Kokushimusou: return 'kokushimusou';
-    case Yaku.Chuurenpoto9Sides: return 'chuurenpoto 9 sides';
-    case Yaku.Chuurenpoto: return 'chuurenpoto';
-    case Yaku.SuuankouTanki: return 'suuankou tanki';
-    case Yaku.Suuankou: return 'suuankou';
-    case Yaku.Daisuushi: return 'daisuushi';
-    case Yaku.Shosuushi: return 'shosuushi';
-    case Yaku.Daisangen: return 'daisangen';
-    case Yaku.Tsuuiisou: return 'tsuuiisou';
-    case Yaku.Ryuuiisou: return 'ryuuiisou';
-    case Yaku.Chinroutou: return 'chinroutou';
-    case Yaku.Suukantsu: return 'suukantsu';
-    case Yaku.Tenhou: return 'tenhou';
-    case Yaku.Chihou: return 'chihou';
-    case Yaku.Renhou: return 'renhou';
-    case Yaku.Daisharin: return '';
-    case Yaku.Chinitsu: return 'chinitsu';
-    case Yaku.Honitsu: return 'honitsu';
-    case Yaku.Ryanpeikou: return 'ryanpeikou';
-    case Yaku.Junchan: return 'junchan';
-    case Yaku.Chanta: return 'chanta';
-    case Yaku.Toitoi: return 'toitoi';
-    case Yaku.Honroutou: return 'honroutou';
-    case Yaku.Sankantsu: return 'sankantsu';
-    case Yaku.Shosangen: return 'shosangen';
-    case Yaku.SanshokuDoukou: return 'sanshoku doukou';
-    case Yaku.Sanankou: return 'sanankou';
-    case Yaku.Chiitoitsu: return 'chiitoitsu';
-    case Yaku.DaburuRiichi: return 'daburu riichi';
-    case Yaku.Ittsu: return 'ittsu';
-    case Yaku.Sanshoku: return 'sanshoku';
-    case Yaku.Tanyao: return 'tanyao';
-    case Yaku.Pinfu: return 'pinfu';
-    case Yaku.Iipeikou: return 'iipeikou';
-    case Yaku.Menzentsumo: return 'menzentsumo';
-    case Yaku.Riichi: return 'riichi';
-    case Yaku.Ippatsu: return 'ippatsu';
-    case Yaku.Rinshan: return 'rinshan';
-    case Yaku.Chankan: return 'chankan';
-    case Yaku.Haitei: return 'haitei';
-    case Yaku.Houtei: return 'houtei';
-    case Yaku.RoundWindEast: return 'round wind east';
-    case Yaku.RoundWindSouth: return 'round wind south';
-    case Yaku.RoundWindWest: return 'round wind west';
-    case Yaku.RoundWindNorth: return 'round wind north';
-    case Yaku.OwnWindEast: return 'own wind east';
-    case Yaku.OwnWindSouth: return 'own wind south';
-    case Yaku.OwnWindWest: return 'own wind west';
-    case Yaku.OwnWindNorth: return 'own wind north';
-    case Yaku.Haku: return 'haku';
-    case Yaku.Hatsu: return 'hatsu';
-    case Yaku.Chun: return 'chun';
-    case Yaku.Dora: return 'dora';
-    case Yaku.Uradora: return 'uradora';
-    case Yaku.Akadora: return 'akadora';
-    default: return '';
+    case Yaku.Kokushimusou13Sides:
+      return 'kokushimusou 13 sides';
+    case Yaku.Kokushimusou:
+      return 'kokushimusou';
+    case Yaku.Chuurenpoto9Sides:
+      return 'chuurenpoto 9 sides';
+    case Yaku.Chuurenpoto:
+      return 'chuurenpoto';
+    case Yaku.SuuankouTanki:
+      return 'suuankou tanki';
+    case Yaku.Suuankou:
+      return 'suuankou';
+    case Yaku.Daisuushi:
+      return 'daisuushi';
+    case Yaku.Shosuushi:
+      return 'shosuushi';
+    case Yaku.Daisangen:
+      return 'daisangen';
+    case Yaku.Tsuuiisou:
+      return 'tsuuiisou';
+    case Yaku.Ryuuiisou:
+      return 'ryuuiisou';
+    case Yaku.Chinroutou:
+      return 'chinroutou';
+    case Yaku.Suukantsu:
+      return 'suukantsu';
+    case Yaku.Tenhou:
+      return 'tenhou';
+    case Yaku.Chihou:
+      return 'chihou';
+    case Yaku.Renhou:
+      return 'renhou';
+    case Yaku.Daisharin:
+      return '';
+    case Yaku.Chinitsu:
+      return 'chinitsu';
+    case Yaku.Honitsu:
+      return 'honitsu';
+    case Yaku.Ryanpeikou:
+      return 'ryanpeikou';
+    case Yaku.Junchan:
+      return 'junchan';
+    case Yaku.Chanta:
+      return 'chanta';
+    case Yaku.Toitoi:
+      return 'toitoi';
+    case Yaku.Honroutou:
+      return 'honroutou';
+    case Yaku.Sankantsu:
+      return 'sankantsu';
+    case Yaku.Shosangen:
+      return 'shosangen';
+    case Yaku.SanshokuDoukou:
+      return 'sanshoku doukou';
+    case Yaku.Sanankou:
+      return 'sanankou';
+    case Yaku.Chiitoitsu:
+      return 'chiitoitsu';
+    case Yaku.DaburuRiichi:
+      return 'daburu riichi';
+    case Yaku.Ittsu:
+      return 'ittsu';
+    case Yaku.Sanshoku:
+      return 'sanshoku';
+    case Yaku.Tanyao:
+      return 'tanyao';
+    case Yaku.Pinfu:
+      return 'pinfu';
+    case Yaku.Iipeikou:
+      return 'iipeikou';
+    case Yaku.Menzentsumo:
+      return 'menzentsumo';
+    case Yaku.Riichi:
+      return 'riichi';
+    case Yaku.Ippatsu:
+      return 'ippatsu';
+    case Yaku.Rinshan:
+      return 'rinshan';
+    case Yaku.Chankan:
+      return 'chankan';
+    case Yaku.Haitei:
+      return 'haitei';
+    case Yaku.Houtei:
+      return 'houtei';
+    case Yaku.RoundWindEast:
+      return 'round wind east';
+    case Yaku.RoundWindSouth:
+      return 'round wind south';
+    case Yaku.RoundWindWest:
+      return 'round wind west';
+    case Yaku.RoundWindNorth:
+      return 'round wind north';
+    case Yaku.OwnWindEast:
+      return 'own wind east';
+    case Yaku.OwnWindSouth:
+      return 'own wind south';
+    case Yaku.OwnWindWest:
+      return 'own wind west';
+    case Yaku.OwnWindNorth:
+      return 'own wind north';
+    case Yaku.Haku:
+      return 'haku';
+    case Yaku.Hatsu:
+      return 'hatsu';
+    case Yaku.Chun:
+      return 'chun';
+    case Yaku.Dora:
+      return 'dora';
+    case Yaku.Uradora:
+      return 'uradora';
+    case Yaku.Akadora:
+      return 'akadora';
+    default:
+      return '';
   }
 }
